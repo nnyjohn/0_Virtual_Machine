191b0915dba782ff95bd05c167bea9074083bb98
Johnathan Sattler <jsattler@outlook.com>
2016-08-31 17:55:47 -0400

    added code to push to, and print codeStack

--- vm.c
+++ vm.c
@@ -2,13 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define FILE_SIZE 512
+
 struct codeStack
 {
 	int op;
 	int lvl;
 	int mod;
-	struct CodeStack * next;
-} * code;
+	struct codeStack * next;
+} * code, * pc;
 
 struct memStack
 {
@@ -16,16 +18,79 @@ struct memStack
 	struct memStack * next;
 } * stack;
 
+void readFile(const char * fileName);
+void pushCode(int comp[]);
+void printCode();
+
 int main(int argc, const char * argv[]) {
 	
-	char * fileIn;
+	const char * fileName = argv[1];
+
+	readFile(fileName);
+
+	printCode();
+
+	return 0;
+}
+
+void readFile(const char * fileName) {
+
 	FILE * ifp;
+	char line[FILE_SIZE + 1], temp;
+	int i = 0, data;
+	int comp[3];
 
-	ifp = fopen(argv[1], "r");
+	ifp = fopen(fileName, "r");
 
-	printf("Hello World!\n");
+	if (ifp == NULL) {
+		printf("Error: File not found\n");
+		exit(0);
+	}
+
+	while(fscanf(ifp, "%d", &data) != EOF) {
+		//printf("%d ", data);
+
+		comp[i++ % 3] = data;
+
+		if (i % 3 == 0 && i != 0) {
+			//printf("\n");
+			pushCode(comp);
+		}
+	}
 
 	fclose(ifp);
 
-	return 0;
+	return;
+}
+
+void pushCode(int comp[]) {
+
+	struct codeStack * node = (struct codeStack *) malloc(sizeof(struct codeStack));
+	struct codeStack * last = code;
+
+	node -> op = comp[0];
+	node -> lvl = comp[1];
+	node -> mod = comp[2];
+	node -> next = NULL;
+
+	if (code == NULL) {
+		code = node;
+	} else {
+		while (last -> next != NULL)
+			last = last -> next;
+
+		last -> next = node;
+	}
+
+	return;
+}
+
+void printCode() {
+
+	struct codeStack * current = code;
+
+	while (current != NULL) {
+		printf("%d %d %d\n", current -> op, current -> lvl, current -> mod);
+		current = current -> next;
+	}
 }
