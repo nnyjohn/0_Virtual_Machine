5f4778e11696ee8bbab6e865800a30db670dbf09
Johnathan Sattler <jsattler@outlook.com>
2016-08-31 16:59:55 -0400

    Updated stacks

--- vm.c
+++ vm.c
@@ -1,13 +1,34 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-typedef struct Stack
+struct codeStack
 {
-	Stack * node;
-} Stack;
+	int op;
+	int lvl;
+	int mod;
+	struct CodeStack * next;
+} * code;
 
-int main() {
+struct memStack
+{
+	int info;
+	struct memStack * next;
+} * stack;
+
+int main(int argc, const char * argv[]) {
 	
+	char * fileIn;
+	FILE * ifp;
+	FILE * ofp;
+
+	ifp = fopen(argv[1], "r");
+	ofp = fopen(argv[2], "r");
+
 	printf("Hello World!\n");
+
+	fclose(ifp);
+	fclose(ofp);
 
 	return 0;
 }
