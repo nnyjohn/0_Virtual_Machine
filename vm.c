d4a61339496395fbdd3a53355fcbd2815334690b
Johnathan Sattler <jsattler@outlook.com>
2016-09-19 18:15:18 -0400

    Added collaboration file

--- vm.c
+++ vm.c
@@ -1,3 +1,10 @@
+// Compiler Builder 8
+// Lester Young
+// Timothy Konzel
+// Remington Howell
+// Johnathan Sattler
+// John Herold
+
 // Included libraries
 #include <stdio.h>
 #include <string.h>
